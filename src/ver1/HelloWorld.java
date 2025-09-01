fe30c8d73950929d69bc0dffebb6d80a3d0dd5f2
Ryan Samet <ryan.samet95@gmail.com>
2025-09-01 17:11:24 -0400

    Ryan is working on his new branch locally

--- src/ver1/HelloWorld.java
+++ src/ver1/HelloWorld.java
@@ -3,6 +3,6 @@ package ver1;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello from IntelliJ");
-        System.out.println("Ryan is working locally now");
+        System.out.println("Ryan is working locally on his branch");
     }
 }
